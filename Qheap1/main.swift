793f4b8ded436e18d64727d7a3799abb1fed484a
slava bily <slavabily@me.com>
2021-12-17 16:13:41 +0200

    the sample test case is passed

--- Qheap1/main.swift
+++ Qheap1/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+var heap = [Int]()
+
+//print("Please, enter the number of queries")
+
+if let Q = readLine() {
+    let q = Int(Q)!
+    var n = 0
+    
+    while n < q {
+//        print("Please, enter query")
+        if let query = readLine() {
+            let prefix = query.prefix(1)
+            
+            var i: Int {
+                var suff = query.suffix(from: prefix.endIndex)
+                suff.removeFirst()
+                return Int(suff)!
+            }
+            
+            switch prefix {
+            case "1":
+                heap.append(i)
+            case "2":
+                heap.remove(at: heap.firstIndex(of: i)!)
+            case "3":
+                let min = heap.min()!
+                print(min)
+            default:
+                break
+            }
+//            print(heap)
+        }
+        n += 1
+    }
+}
+
+
 
